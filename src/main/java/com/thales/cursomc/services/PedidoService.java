2c82df9b790291e2d250888d35102b78f3a524ad
Thales Henrique <thaalesheenrique@gmail.com>
2019-06-13 15:55:15 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/thales/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.thales.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thales.cursomc.domain.Pedido;
+import com.thales.cursomc.repositories.PedidoRepository;
+import com.thales.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + 
+					", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
