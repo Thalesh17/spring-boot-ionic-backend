cafb6e2ef2beac0244f8eb3652aa7d6b0ec39248
Thales Henrique <thaalesheenrique@gmail.com>
2019-06-17 10:59:56 -0300

    Validacao customizada: email não repitido na atualizacao de Cliente

--- src/main/java/com/thales/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/thales/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.thales.cursomc.domain.Cliente;
+import com.thales.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
